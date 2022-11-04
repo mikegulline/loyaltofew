9b036ddd073248db4a017e5003b031e4082aed9f
Mike Gulline <mike@mikegulline.com>
2022-11-03 17:02:26 -0700

    updated useProducts to use source over api

--- hooks/useProducts.js
+++ hooks/useProducts.js
@@ -1,20 +1,10 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { newStore } from '../data/store';
 import { useRouter } from 'next/router';
 
 const useProducts = () => {
-  const [products, setProducts] = useState([]);
   const router = useRouter();
   const slug = router.query.slug;
-
-  useEffect(() => {
-    if (slug) {
-      (async () => {
-        const { data } = await axios.get(`/api/products/${slug.join('/')}`);
-        setProducts(data);
-      })();
-    }
-  }, [setProducts, slug]);
+  const products = (slug && newStore(...slug)) || [];
 
   return products;
 };
