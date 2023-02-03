51b08629a6b4803e27cc73c02d1e2481fa81cd5d
Mike Gulline <mike@mikegulline.com>
2023-02-03 12:35:03 -0800

    final update to rate webhook

--- pages/api/webhooks/snipcart-shipping.js
+++ pages/api/webhooks/snipcart-shipping.js
@@ -39,87 +39,17 @@ handler.get(async (req, res) => {
 });
 
 handler.post(async (req, res) => {
-  // convert Snipcart vars to EasyPost vars
-  const {
-    eventName = '',
-    content: {
-      shippingAddressName: name = '',
-      shippingAddressCompanyName: company = '',
-      shippingAddressAddress1: street1 = '',
-      shippingAddressAddress2: street2 = '',
-      shippingAddressCity: city = '',
-      shippingAddressCountry: country = '',
-      shippingAddressProvince: state = '',
-      shippingAddressPostalCode: zip = '',
-      shippingAddressPhone: phone = '',
-      // totalWeight: weight = 33.3,
-    },
-  } = req.body;
+  // get rates or errors
+  const { rates, errors } = await getRates(req.body);
 
-  // try to get rates
-  try {
-    // init EasyPost API
-    const api = new EasyPost(API_KEY);
-
-    // build shipment
-    const shipment = new api.Shipment({
-      from_address: {
-        street1: '417 MONTGOMERY ST',
-        street2: 'FLOOR 5',
-        city: 'SAN FRANCISCO',
-        state: 'CA',
-        zip: '94104',
-        country: 'US',
-        company: 'EasyPost',
-        phone: '415-123-4567',
-      },
-      to_address: {
-        name,
-        company,
-        street1,
-        street2,
-        city,
-        country,
-        state,
-        zip,
-        phone,
-      },
-      parcel: {
-        weight: 33.3,
-      },
-    });
-
-    const save = await shipment.save();
-    // get lowest rate
-    // const lowestRate = shipment.lowestRate();
-
-    // map rates
-    const rates = save.rates
-      .map(({ rate }) => {
-        return {
-          cost: Number(rate),
-          description: `$${rate} shipping`,
-        };
-      })
-      .sort((a, b) => a.cost - b.cost);
-
-    return res.json({ rates });
-  } catch (errors) {
+  // return errors
+  if (errors) {
     return res.json({ errors });
   }
+
+  // return rates
+  return res.json({ rates });
 });
-// handler.post(async (req, res) => {
-//   // get rates or errors
-//   const { rates, errors } = await getRates(req.body);
-
-//   // return errors
-//   if (errors) {
-//     return res.json({ errors });
-//   }
-
-//   // return rates
-//   return res.json({ rates });
-// });
 
 ///////////////////////////////////
 ///////////////////////////////////
@@ -190,7 +120,8 @@ async function getRates(body) {
           description: `$${rate} shipping`,
         };
       })
-      .sort((a, b) => a.cost - b.cost);
+      .sort((a, b) => a.cost - b.cost)
+      .slice(0, 3);
 
     return { rates, errors: null };
   } catch (errors) {
