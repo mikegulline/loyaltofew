bbc9dc6ca099f248266440ec4fa2ed688117aee9
Mike Gulline <mike@mikegulline.com>
2023-02-03 10:57:21 -0800

    rate test

--- pages/api/webhooks/snipcart-shipping.js
+++ pages/api/webhooks/snipcart-shipping.js
@@ -9,38 +9,106 @@ const API_KEY = process.env.EASYPOST_API_TEST;
 
 const handler = nc();
 
-// handler.get(async (req, res) => {
-//   const body = {
-//     eventName: '',
-//     content: {
-//       shippingAddressName: 'Mike Gulline',
-//       shippingAddressCompanyName: '',
-//       shippingAddressAddress1: '7624 Potter Valley Dr.',
-//       shippingAddressAddress2: '',
-//       shippingAddressCity: 'Eastvale',
-//       shippingAddressCountry: 'USA',
-//       shippingAddressProvince: 'CA',
-//       shippingAddressPostalCode: '92880',
-//       shippingAddressPhone: '',
-//       totalWeight: 33.3,
-//     },
-//   };
+handler.get(async (req, res) => {
+  const body = {
+    eventName: '',
+    content: {
+      shippingAddressName: 'Mike Gulline',
+      shippingAddressCompanyName: '',
+      shippingAddressAddress1: '7624 Potter Valley Dr.',
+      shippingAddressAddress2: '',
+      shippingAddressCity: 'Eastvale',
+      shippingAddressCountry: 'USA',
+      shippingAddressProvince: 'CA',
+      shippingAddressPostalCode: '92880',
+      shippingAddressPhone: '',
+      totalWeight: 33.3,
+    },
+  };
 
-//   // get rates or errors
-//   const { rates, errors } = await getRates(body);
+  // get rates or errors
+  const { rates, errors } = await getRates(body);
 
-//   // return errors
-//   if (errors) {
-//     return res.json({ errors });
-//   }
+  // return errors
+  if (errors) {
+    return res.json({ errors });
+  }
 
-//   // return rates
-//   return res.json({ rates });
-// });
+  // return rates
+  return res.json({ rates });
+});
 
 handler.post(async (req, res) => {
   // get rates or errors
-  const { rates, errors } = await getRates(req.body);
+  // const { rates, errors } = await getRates(req.body);
+
+  const errors = null;
+  const rates = [
+    {
+      cost: 8.14,
+      description: '$8.14 shipping',
+    },
+    {
+      cost: 8.31,
+      description: '$8.31 shipping',
+    },
+    {
+      cost: 12.73,
+      description: '$12.73 shipping',
+    },
+    {
+      cost: 19.68,
+      description: '$19.68 shipping',
+    },
+    {
+      cost: 20.78,
+      description: '$20.78 shipping',
+    },
+    {
+      cost: 21.93,
+      description: '$21.93 shipping',
+    },
+    {
+      cost: 23.12,
+      description: '$23.12 shipping',
+    },
+    {
+      cost: 33.16,
+      description: '$33.16 shipping',
+    },
+    {
+      cost: 39.05,
+      description: '$39.05 shipping',
+    },
+    {
+      cost: 40.15,
+      description: '$40.15 shipping',
+    },
+    {
+      cost: 57.62,
+      description: '$57.62 shipping',
+    },
+    {
+      cost: 63.03,
+      description: '$63.03 shipping',
+    },
+    {
+      cost: 104.09,
+      description: '$104.09 shipping',
+    },
+    {
+      cost: 113.22,
+      description: '$113.22 shipping',
+    },
+    {
+      cost: 142.63,
+      description: '$142.63 shipping',
+    },
+    {
+      cost: 149.6,
+      description: '$149.60 shipping',
+    },
+  ];
 
   // return errors
   if (errors) {
