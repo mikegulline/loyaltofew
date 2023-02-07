d583aee1cee3ed59b3591689409a160ec9f86f4b
Mike Gulline <mike@mikegulline.com>
2023-02-07 10:22:10 -0800

    refactored ColorLinks component.

--- components/ColorLinks.jsx
+++ components/ColorLinks.jsx
@@ -1,36 +1,31 @@
 import Link from './Link';
 
-const ColorLinks = ({
-  colors,
-  link,
-  align = '',
-  scroll = true,
-  className = '',
-}) => {
-  const wrapperClass = `flex items-center ${
-    align ? 'justify-left' : 'justify-center'
-  }`;
+const ColorLinks = (props) => {
+  const { colors, link, align = ``, scroll = true, className = `` } = props;
+  const alignClass = align ? `justify-${align}` : `justify-center`;
+  const wrapperClasses = `flex items-center ${alignClass} ${className}`;
 
-  const buildLinks = colors.map((color) => {
-    const linkClasses = `w-8 h-8 rounded-full border solid border-white -mr-1 -ml-1 ${color
-      .toLowerCase()
-      .replace(' ', '')}`;
-    const href = `${link}/${color.toLowerCase().replace(' ', '')}`;
+  return (
+    <div className={wrapperClasses}>
+      {colors.map((color) => {
+        const colorName = color.toLowerCase().replace(` `, ``);
+        const linkClasses = `w-8 h-8 rounded-full border solid border-white -mr-1 -ml-1 ${colorName}`;
+        const href = `${link}/${colorName}`;
 
-    return (
-      <Link
-        className={linkClasses}
-        key={color}
-        href={href}
-        title={color}
-        scroll={scroll}
-      >
-        <div className='hidden'>{color}</div>
-      </Link>
-    );
-  });
-
-  return <div className={wrapperClass + ' ' + className}>{buildLinks}</div>;
+        return (
+          <Link
+            className={linkClasses}
+            key={colorName}
+            href={href}
+            title={color}
+            scroll={scroll}
+          >
+            <div className='hidden'>{color}</div>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ColorLinks;
