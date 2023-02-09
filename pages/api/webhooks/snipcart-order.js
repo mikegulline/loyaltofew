53b877a38fab96f1110412089e10ac183097a220
Mike Gulline <mike@mikegulline.com>
2023-02-09 11:06:13 -0800

    if check

--- pages/api/webhooks/snipcart-order.js
+++ pages/api/webhooks/snipcart-order.js
@@ -6,19 +6,17 @@ const handler = nc();
 
 handler.post(async (req, res) => {
   const saveOrder = { ...req.body };
-  if (saveOrder.eventName === 'order.completed') {
-    try {
-      await db.connectDB();
+  try {
+    await db.connectDB();
 
-      const order = await new Order(saveOrder).save();
+    const order = await new Order(saveOrder).save();
 
-      await db.disconnectDB();
-      return res.json({ message: order });
-    } catch (errors) {
-      return res.status(500).json({ errors });
-    }
+    res.json({ message: order });
+
+    await db.disconnectDB();
+  } catch (errors) {
+    res.status(500).json({ errors });
   }
-  return res.status(200);
 });
 
 export default handler;
