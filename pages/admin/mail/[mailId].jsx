d8e0dd570a02c0f6fd2a7112a59b2fe338bef068
Mike Gulline <mike@mikegulline.com>
2023-02-26 13:36:27 -0800

    added mailId and orderId pages

---
+++ pages/admin/mail/[mailId].jsx
@@ -0,0 +1,4 @@
+import H1 from '../../../components/Type/H1';
+export default function MailId() {
+  return <H1>Mail by Id</H1>;
+}
---
+++ pages/admin/orders/[orderId].jsx
@@ -0,0 +1,4 @@
+import H1 from '../../../components/Type/H1';
+export default function OrderId() {
+  return <H1>Order by Id</H1>;
+}
