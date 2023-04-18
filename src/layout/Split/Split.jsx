bd016fe55410643f77523e1b714e3331714f6d88
Mike Gulline <mike@mikegulline.com>
2023-04-18 15:53:41 -0700

    updated split image css

--- src/layout/Split/Split.jsx
+++ src/layout/Split/Split.jsx
@@ -6,14 +6,17 @@ export default function Split({ children, image }) {
       <div className='ml m-auto w-full px-4 py-10 md:max-w-screen-md lg:max-w-screen-lg xl:mr-0 xl:max-w-[624px] xl:px-0  xl:py-40 xl:pr-32 2xl:max-w-[752px] 2xl:pr-40'>
         {children}
       </div>
-      <Image
-        object-fit='cover'
-        src={image}
-        placeholder='blur'
-        alt='Loyal to Few®'
-        width='2400'
-        height='2400'
-      />
+
+      <div className='relative flex items-center justify-center'>
+        <Image
+          className='absolute h-full w-full object-cover'
+          src={image}
+          placeholder='blur'
+          alt='Loyal to Few®'
+          width='2400'
+          height='2400'
+        />
+      </div>
     </div>
   );
 }
