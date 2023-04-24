09f2791e0053bf3184656e610e4f614fa94174ab
Mike Gulline <mike@mikegulline.com>
2023-04-23 23:16:39 -0700

    updated broken link

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -6,7 +6,7 @@ const Category = ({ products }) =>
   products.map(({ name, logos, colors }) => (
     <SlideshowGridGallery key={name} title={name}>
       {logos.map((logo) => (
-        <GridItem key={logo.name} product={logo}>
+        <GridItem key={logo.name} product={{ ...logo, link: logo.linkColor }}>
           <h4 className='mt-2 mb-2 font-medium'>{`${logo.logo} Design`}</h4>
           <ColorLinks colors={colors} link={logo.link} scroll={true} />
         </GridItem>
