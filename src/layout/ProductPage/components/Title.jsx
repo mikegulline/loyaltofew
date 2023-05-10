271c987023f87da0d554804e1dfb0bbbbe4c6c9f
Mike Gulline <mike@mikegulline.com>
2023-05-09 20:23:11 -0700

    smaller product title on mobile

--- src/layout/ProductPage/components/Title.jsx
+++ src/layout/ProductPage/components/Title.jsx
@@ -1,5 +1,5 @@
 const Title = ({ name }) => (
-  <h1 className='mb-6 text-4xl font-black text-gray-900 lg:text-5xl'>{name}</h1>
+  <h1 className='mb-6 text-2xl font-black text-gray-900 lg:text-5xl'>{name}</h1>
 );
 
 export default Title;
