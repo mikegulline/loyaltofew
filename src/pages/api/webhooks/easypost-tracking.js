23c4050ef7f4a2d3740ba3b05e6438e2d63a02e7
Mike Gulline <mike@mikegulline.com>
2023-05-24 15:07:15 -0700

    no infinite loop

--- src/pages/api/webhooks/easypost-tracking.js
+++ src/pages/api/webhooks/easypost-tracking.js
@@ -4,13 +4,13 @@ import mail from '@/utils/mail';
 const handler = nc();
 
 handler.post(async (req, res) => {
-  // await mail(req, 'easypost-tracking.js');
+  await mail(req, 'easypost-tracking.js');
 
   res.status(200).json({ message: 'check' });
 });
 
 handler.get(async (req, res) => {
-  // await mail(req, 'easypost-tracking.js');
+  await mail(req, 'easypost-tracking.js');
 
   res.status(200).json({ message: 'check' });
 });
