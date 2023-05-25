7a4f4675e86c5d0c5ea9c7410350b8d4ac09c8c9
Mike Gulline <mike@mikegulline.com>
2023-05-25 09:24:06 -0700

    ept update

--- src/pages/api/webhooks/easypost-tracking.js
+++ src/pages/api/webhooks/easypost-tracking.js
@@ -7,15 +7,7 @@ const handler = nc();
 
 handler.post(async (req, res) => {
   const {
-    description,
-    result: {
-      id,
-      status,
-      status_detail,
-      est_delivery_date,
-      tracking_details,
-      public_url,
-    },
+    result: { id, status, tracking_details, public_url },
   } = req.body;
 
   // get SC order token from Mongo by Tracking Id
@@ -42,15 +34,28 @@ handler.post(async (req, res) => {
     const noTracking = !tracking_details.length;
     if (noTracking) return res.status(200).json({ message: 'continue' });
 
-    const info = tracking_details[tracking_details.length - 1];
+    const message = mailMessage(order, tracking_details, public_url);
 
-    const desc = info.description
-      ? `
+    await mail(message, 'easypost-tracking.js');
+  }
+
+  res.status(200).json({ message: 'check' });
+});
+
+//////////////////////
+//////////////////////
+//////////////////////
+//////////////////////
+const mailMessage = (order, tracking_details, public_url) => {
+  const info = tracking_details[tracking_details.length - 1];
+
+  const desc = info.description
+    ? `
 
 ${info.description}`
-      : '';
+    : '';
 
-    const message = `
+  return `
 LTF Order Update for ${order.email}
 
 ${info.message}${desc}
@@ -58,12 +63,7 @@ ${info.message}${desc}
 You can track your shipment status here <a href="${public_url}">${public_url}</a>
 
 Loyal to Few
-    `;
-
-    await mail(message, 'easypost-tracking.js');
-  }
-
-  res.status(200).json({ message: 'check' });
-});
+  `;
+};
 
 export default handler;
