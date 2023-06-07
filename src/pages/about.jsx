50af2ff9b25d85f1355e71eb9766985e80b10b0c
Mike Gulline <mike@mikegulline.com>
2023-06-07 09:52:55 -0700

    update about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -16,28 +16,28 @@ const About = () => {
 
         <P>
           Loyal To Few® is a family-owned business started by a retired
-          firefighter/paramedic. What began as a mindset for survival, turned
-          into a tattoo, and eventually evolved into a one-of-a-kind clothing
-          brand that men and women of all ages who have overcome adversity can
-          relate to.
+          firefighter/paramedic and his older, much smarter brother. What began
+          as a mindset for survival, turned into a tattoo, and eventually
+          evolved into a one-of-a-kind clothing brand that men and women of all
+          ages who have overcome adversity can relate to.
         </P>
 
         <P>
-          Sometimes in life you realize you have let people in who don&apos;t
-          want the best for you, who take and never give. Surrounding yourself
-          with family and a small circle of friends who will remain in your
+          Sometimes in life, we realize that we have let people in who don't
+          want the best for you, who take and never give. Surrounding ourselves
+          with family and a small circle of friends who will remain in our
           corner no matter how difficult the circumstances has become our
-          trademarked way of life. Trim the fat, divorce the bullshit, and
-          don&apos;t waste time or energy on people who hinder your personal
-          growth and success.
+          trademarked way of life. We trim the fat, divorce the bullshit, and
+          don't waste time or energy on people who hinder our personal growth
+          and success.
         </P>
 
         <P>
-          A life without hardships is inevitable, but you can meet any challenge
-          if you surround yourself with people who have your best interests in
-          mind and will never turn their backs on you. When the dust settles and
-          you see the ones still standing by your side, you have found your
-          Loyal to Few®.
+          A life with hardships is inevitable, but you can meet any challenge if
+          you surround yourself with people who have your best interests in mind
+          and will never turn their backs on you. When the dust settles and you
+          see the ones still standing by our side, you have found your Loyal to
+          Few®.
         </P>
       </InfoSplit>
     </>
