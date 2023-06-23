5b27a2f3d44ea2a14bc475f3f462494796112d4a
Mike Gulline <mike@mikegulline.com>
2023-06-23 14:48:54 -0700

    remove inventory cron

--- src/pages/api/webhooks/inventory-chron.js
+++ src/pages/api/webhooks/inventory-chron.js
@@ -1,86 +1,86 @@
-import Order from '@/models/order';
-import db from '@/utils/db';
-import mail from '@/utils/mail';
+// import Order from '@/models/order';
+// import db from '@/utils/db';
+// import mail from '@/utils/mail';
 
-export default async function handler(req, res) {
-  const today = new Date();
-  const hours = today.getHours() - 7;
-  const canRun = hours === 0;
+// export default async function handler(req, res) {
+//   const today = new Date();
+//   const hours = today.getHours() - 7;
+//   const canRun = hours === 0;
 
-  if (!process.env.CHRON) {
-    console.log('no chron');
-    return res.status(200).json({ message: 'no chron' });
-  }
+//   if (!process.env.CHRON) {
+//     console.log('no chron');
+//     return res.status(200).json({ message: 'no chron' });
+//   }
 
-  if (!canRun) {
-    console.log('bad time ' + hours);
-    return res.status(200).json({ message: 'bad time', hours });
-  }
+//   if (!canRun) {
+//     console.log('bad time ' + hours);
+//     return res.status(200).json({ message: 'bad time', hours });
+//   }
 
-  if (!process.env.RETURNS_EMAIL) {
-    console.log('no returns email');
-    return res.status(200).json({ message: 'bad email' });
-  }
+//   if (!process.env.RETURNS_EMAIL) {
+//     console.log('no returns email');
+//     return res.status(200).json({ message: 'bad email' });
+//   }
 
-  let subject, message;
-  try {
-    await db.connectDB();
+//   let subject, message;
+//   try {
+//     await db.connectDB();
 
-    const previousDay = new Date();
-    previousDay.setDate(previousDay.getDate() - 1);
+//     const previousDay = new Date();
+//     previousDay.setDate(previousDay.getDate() - 1);
 
-    const orders = await Order.find({
-      createdAt: { $gte: previousDay },
-    }).exec();
+//     const orders = await Order.find({
+//       createdAt: { $gte: previousDay },
+//     }).exec();
 
-    if (!orders.length) {
-      subject = `LTF Inventory Update`;
-      message = `<p>No orders in the past 24hrs :(</p>
-      <p>Let's fix that!</p>
-      <p>LTF Robot</p>
-      `;
+//     if (!orders.length) {
+//       subject = `LTF Inventory Update`;
+//       message = `<p>No orders in the past 24hrs :(</p>
+//       <p>Let's fix that!</p>
+//       <p>LTF Robot</p>
+//       `;
 
-      await mail(process.env.RETURNS_EMAIL, subject, message);
-      await mail('mike@mikegulline.com', subject, message);
+//       await mail(process.env.RETURNS_EMAIL, subject, message);
+//       await mail('mike@mikegulline.com', subject, message);
 
-      return res.status(200).json({ message: 'update sent' });
-    }
+//       return res.status(200).json({ message: 'update sent' });
+//     }
 
-    const returnOrders = {};
-    let countOrders = 0;
+//     const returnOrders = {};
+//     let countOrders = 0;
 
-    orders.map(({ items }) => {
-      items.map(({ baseId, quantity }) => {
-        countOrders += quantity;
-        if (baseId in returnOrders) {
-          returnOrders[baseId] += quantity;
-        } else {
-          returnOrders[baseId] = quantity;
-        }
-      });
-    });
+//     orders.map(({ items }) => {
+//       items.map(({ baseId, quantity }) => {
+//         countOrders += quantity;
+//         if (baseId in returnOrders) {
+//           returnOrders[baseId] += quantity;
+//         } else {
+//           returnOrders[baseId] = quantity;
+//         }
+//       });
+//     });
 
-    let htmlOrders = '';
+//     let htmlOrders = '';
 
-    for (const key in returnOrders) {
-      htmlOrders += `<p> - (${returnOrders[key]}) ${key}</p>`;
-    }
+//     for (const key in returnOrders) {
+//       htmlOrders += `<p> - (${returnOrders[key]}) ${key}</p>`;
+//     }
 
-    subject = `LTF Inventory Update`;
-    message = `<p>You got ${countOrders} orders in the past 24hrs!</p>
-    <p><strong>Time to place an order for:</strong></p>
-    ${htmlOrders}
-    <p>Keep it up, Champ!</p>
-    <p>LTF Robot</p>
-    `;
-    console.log('sending emails');
-    await mail(process.env.RETURNS_EMAIL, subject, message);
-    await mail('mike@mikegulline.com', subject, message);
+//     subject = `LTF Inventory Update`;
+//     message = `<p>You got ${countOrders} orders in the past 24hrs!</p>
+//     <p><strong>Time to place an order for:</strong></p>
+//     ${htmlOrders}
+//     <p>Keep it up, Champ!</p>
+//     <p>LTF Robot</p>
+//     `;
+//     console.log('sending emails');
+//     await mail(process.env.RETURNS_EMAIL, subject, message);
+//     await mail('mike@mikegulline.com', subject, message);
 
-    return res.status(200).json({ message: 'update sent' });
-  } catch (error) {
-    return res.status(500).json(error);
-  } finally {
-    await db.disconnectDB();
-  }
-}
+//     return res.status(200).json({ message: 'update sent' });
+//   } catch (error) {
+//     return res.status(500).json(error);
+//   } finally {
+//     await db.disconnectDB();
+//   }
+// }
