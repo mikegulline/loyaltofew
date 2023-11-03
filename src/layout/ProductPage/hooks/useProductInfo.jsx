f4630085614308da9568f90e30129faba23aac4c
Mike Gulline <mike@mikegulline.com>
2023-11-03 08:59:10 -0700

    fixed sizes bug

--- src/layout/ProductPage/hooks/useProductInfo.jsx
+++ src/layout/ProductPage/hooks/useProductInfo.jsx
@@ -3,7 +3,30 @@ import { useState, useEffect, useCallback } from 'react';
 const useProductInfo = (product) => {
   const { name, color, colors, logo, logos, sizes, details, link } = product;
 
-  // current color sizeIndex
+  const [sizesArray, setSizesArray] = useState(() =>
+    sizes.filter((size) => {
+      if ('colors' in size && !size?.colors?.includes(color)) {
+        size['active'] = false;
+      } else {
+        size['active'] = true;
+      }
+      return size;
+    })
+  );
+
+  useEffect(() => {
+    setSizesArray(
+      sizes.filter((size) => {
+        if ('colors' in size && !size?.colors?.includes(color)) {
+          size['active'] = false;
+        } else {
+          size['active'] = true;
+        }
+        return size;
+      })
+    );
+  }, [color, sizes]);
+
   const [sizeIndex, setSizeIndex] = useState(0);
 
   const getActiveColors = useCallback(() => {
@@ -13,48 +36,17 @@ const useProductInfo = (product) => {
 
   const [activeColors, setActiveColors] = useState(getActiveColors);
 
-  // sizes array
-  const sizesArray = sizes.filter((size) => {
-    if ('colors' in size && !size?.colors?.includes(color)) {
-      size['active'] = false;
-    } else {
-      size['active'] = true;
-    }
-    return size;
-  });
-
-  const handleSetActiveColors = useCallback(
-    (i) => {
-      const updateColors = sizesArray[i]?.colors;
-      if (updateColors) {
-        const newColors = getActiveColors().filter(
-          (c) => updateColors.indexOf(c) >= 0
-        );
-        // setActiveColors(newColors);
-      } else {
-        setActiveColors(getActiveColors);
-        // setActiveColors(getActiveColors);
-      }
-    },
-    [getActiveColors, sizesArray]
-  );
-
   useEffect(() => {
-    console.log('change size index');
-    handleSetActiveColors(sizeIndex);
-  }, [sizeIndex, handleSetActiveColors]);
-
-  // update colors array based on size
-  // useEffect(() => {
-  //   const updateColors = sizesArray[sizeIndex]?.colors;
-  //   if (updateColors) {
-  //     setActiveColors(
-  //       getActiveColors().filter((c) => updateColors.indexOf(c) >= 0)
-  //     );
-  //   } else {
-  //     setActiveColors(getActiveColors);
-  //   }
-  // }, [sizeIndex, sizesArray, getActiveColors]);
+    const updateColors = sizesArray[sizeIndex]?.colors;
+    if (updateColors) {
+      const newColors = getActiveColors().filter(
+        (c) => updateColors.indexOf(c) >= 0
+      );
+      setActiveColors(newColors);
+    } else {
+      setActiveColors(getActiveColors);
+    }
+  }, [sizeIndex, sizesArray, getActiveColors]);
 
   ////////////////////
   ////////////////////
